85f3185dcaf923e30d95a537660dbaa129653a62
Anton <a.yriev@teleport.run>
2025-07-08 14:29:39 +0300

    Flutter V2 embedding

--- android/src/main/java/com/rhyme/r_scan/RScanView/RScanViewFactory.java
+++ android/src/main/java/com/rhyme/r_scan/RScanView/RScanViewFactory.java
@@ -10,26 +10,22 @@ import io.flutter.plugin.common.StandardMessageCodec;
 import io.flutter.plugin.platform.PlatformView;
 import io.flutter.plugin.platform.PlatformViewFactory;
 import io.flutter.view.TextureRegistry;
-import io.flutter.plugin.platform.PlatformViewRegistry;
 
 public class RScanViewFactory extends PlatformViewFactory {
     private final Activity activity;
     private final BinaryMessenger messenger;
     private final RScanPermissions permissions;
     private final TextureRegistry textureRegistry;
-    private final PlatformViewRegistry viewRegistry;
 
     public RScanViewFactory(Activity activity,
                             BinaryMessenger messenger,
                             RScanPermissions permissions,
-                            TextureRegistry textureRegistry,
-                            PlatformViewRegistry viewRegistry) {
+                            TextureRegistry textureRegistry) {
         super(StandardMessageCodec.INSTANCE);
         this.activity = activity;
         this.messenger = messenger;
         this.permissions = permissions;
         this.textureRegistry = textureRegistry;
-        this.viewRegistry = viewRegistry;
     }
 
     @Override
